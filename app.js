5f284f6ff259464abef2c867061082ccc1d2ac70
Grant Timmerman <granttimmerman@gmail.com>
2014-03-10 16:51:54 -0700

    Updated html and stripped socketio for testing

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var app = require('express')()
 
 server.listen(8000);
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
