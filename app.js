17f46c263298bc0bbb866314a614b49be8b5040c
Grant Timmerman <granttimmerman@gmail.com>
2014-03-10 16:55:18 -0700

    Uncommented socketio

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.get('*', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-// io.sockets.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
