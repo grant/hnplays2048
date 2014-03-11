30bc60f5b6ceefcf5864972f0229bcc017e2554f
Grant Timmerman <granttimmerman@gmail.com>
2014-03-10 17:29:53 -0700

    Added basic game

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/public'));
 
 io.configure('production', function(){
-  console.log(doodlr + " set config for production");
   io.enable('browser client minification');  // send minified client
   io.enable('browser client etag');          // apply etag caching logic based on version number
   io.enable('browser client gzip');          // gzip the file
@@ -19,6 +21,7 @@ io.configure('production', function(){
 
 var port = process.env.PORT || 8000;
 server.listen(port);
+console.log("Listening at port: " + port);
 
 app.get('*', function (req, res) {
   res.sendfile(__dirname + '/index.html');
---
+++ public/js/application.js
@@ -0,0 +1,3 @@
+window.requestAnimationFrame(function () {
+  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+});
