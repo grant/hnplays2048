086bff1df5590d60608c03fc1b93abb27059fb61
Grant Timmerman <granttimmerman@gmail.com>
2014-03-10 17:02:05 -0700

    Added socket configs

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+io.configure('production', function(){
+  console.log(doodlr + " set config for production");
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 1);                    // reduce logging
+  io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+      'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+  ]);
+});
+
 var port = process.env.PORT || 8000;
 server.listen(port);
 
