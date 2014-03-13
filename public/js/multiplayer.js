62b8e0ad0b4eba7b0fc37e63ba5b633a5226eb71
Grant Timmerman <granttimmerman@gmail.com>
2014-03-13 03:22:32 -0700

    Added the user input list

--- public/js/multiplayer.js
+++ public/js/multiplayer.js
@@ -1,3 +1,8 @@
+var moveList = document.getElementsByClassName("inputlist")[0];
+var highscores = document.getElementsByClassName("scorelist")[0];
+var arrows = ['▲', '▶', '▼', '◀'];
+var MOVE_LIST_CUTOFF = 20;
+
 var socket = io.connect();
 socket.on('connected', function (data) {
   // var userId = data.userId;
@@ -6,9 +11,22 @@ socket.on('connected', function (data) {
 });
 
 socket.on('move', function (data) {
-  // manager.move(data, true);
-  // var direction = data.direction;
-  // var userId = data.userId;
+  // Add move to input list
+  var direction = data.direction;
+  var userId = data.userId;
+  var moveElement = document.createElement('li');
+  moveElement.innerHTML = '<span class="move move-'+direction+'">' + arrows[direction] + '</span>User ' + userId;
+  moveList.insertBefore(moveElement,moveList.firstChild);
+
+  // Remove input list item if there are too many
+  var moveListLen = moveList.childNodes.length;
+  if (moveListLen > MOVE_LIST_CUTOFF) {
+    for (var i = MOVE_LIST_CUTOFF; i < moveListLen; ++i) {
+      moveList.removeChild(moveList.childNodes[i]);
+    }
+  }
+
+  // Set the game state
   if (!manager.over) {
     var gameData = data.gameData;
     manager.setGameData(gameData);
@@ -20,6 +38,7 @@ socket.on('restart', function (gameData) {
   manager.setGameData(gameData);
 });
 
+// Yes, this is in the global scope.
 var Multiplayer = {
   move: function (direction) {
     socket.emit('move', direction);
